23f32a6f37ad9bff4e84fe1043b6adbdff3621aa
Lesley Ying <sy4405@nyu.edu>
2026-02-11 11:59:23 +0800

    port

--- fish chat/public/script.js
+++ fish chat/public/script.js
@@ -1,5 +1,9 @@
-let socket = io();
-
+//let socket = io();
+if(location.hostname.toLowerCase().startsWith('browsercircus') || location.hostname.toLowerCase().startsWith('www')){
+    socket = io({path: "/lesley/port-4290/socket.io"});  // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+  }else{
+    socket = io(); 
+  }
 let formeElm = document.querySelector("#chatForm");
 let msgInput = document.querySelector("#newMessage");
 let waterArea = document.querySelector("#threadWrapper");

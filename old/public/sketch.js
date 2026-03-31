38ae3077a8e66b084e430b3ea2b7277260e60855
Lesley Ying <sy4405@nyu.edu>
2026-03-31 22:21:47 +0800

    kkk

--- old/public/sketch.js
+++ old/public/sketch.js
@@ -124,6 +124,8 @@ function setup() {
     window.visualViewport.addEventListener("scroll", updateUILayout);
     updateUILayout();
   }
+
+  imgs = shuffle(imgs);
 }
 
 function draw() {
@@ -225,8 +227,8 @@ class MyPoint {
     this.emojiAdjustAngle = radians(45);
   }
   update() {
-    this.x = lerp(this.x, this.goalX, 0.2);
-    this.y = lerp(this.y, this.goalY, 0.2);
+    this.x = this.goalX; // lerp(this.x, this.goalX, 0.2);
+    this.y = this.goalY; //lerp(this.y, this.goalY, 0.2);
   }
   updateHeading(h) {
     this.heading = h;
@@ -255,7 +257,7 @@ class MyPoint {
     noStroke();
     textAlign(LEFT);
     textSize(10);
-    text("±" + nf(this.accuracy, 1, 1) + "m", diameter / 2 + 5, 0);
+    //text("±" + nf(this.accuracy, 1, 1) + "m", diameter / 2 + 5, 0);
     pop();
   }
 }
@@ -621,6 +623,7 @@ function shareAllGroups() {
       .then((r) => r.json())
       .then((data) => {
         if (data.ok) console.log("Kite sent to sky screen!", data.kite.id);
+        window.open("sky.html", "_self");
       })
       .catch((err) => console.error("Failed to send kite:", err));
 
@@ -628,8 +631,6 @@ function shareAllGroups() {
   }
 
   if (sent == 0) alert("Stitch some pieces together first!");
-
-  window.open("sky.html", "_self");
 }
 
 function getGroupOf(c) {
@@ -730,5 +731,5 @@ function updateUILayout() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  //resizeCanvas(windowWidth, windowHeight);
 }
--- thisisit/public/sketch.js
+++ thisisit/public/sketch.js
@@ -46,7 +46,6 @@ let mappa_options = {
   style:
     "https://webst01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=6&x={x}&y={y}&z={z}",
 };
-//...
 
 //icon imgs
 function preload() {

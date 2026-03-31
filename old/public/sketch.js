61d6760e39dc34c4f14b81d97149de09bd381884
Lesley Ying <sy4405@nyu.edu>
2026-03-31 22:35:32 +0800

    jjjj

--- old/public/sketch.js
+++ old/public/sketch.js
@@ -615,7 +615,7 @@ function shareAllGroups() {
     gfx.remove();
 
     // POST to server so it saves + broadcasts to sky screen
-    fetch("/kites", {
+    fetch("kites", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ imageData }),

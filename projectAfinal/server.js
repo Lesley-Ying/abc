d369f2f37e59e143a2e3bd368d1ff2b882545789
Lesley Ying <sy4405@nyu.edu>
2026-05-08 17:17:45 +0800

    1

--- projectAfinal/server.js
+++ projectAfinal/server.js
@@ -5,7 +5,7 @@ const https = require("https");
 const fs = require("fs");
 
 const app = express(); // the server "app", the server behaviour
-const portHTTPS = 4290; // port for https
+const portHTTPS = 4291; // port for https
 
 // returning to the client anything that is
 // inside the public folder
